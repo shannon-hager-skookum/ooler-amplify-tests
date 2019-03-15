9bcc32277d8c019d05686bee31557e53bfa2b81d
Shannon Hager <shannon.hager@skookum.com>
2019-03-14 21:19:56 -0400

    set up new amplify project

---
+++ 1/amplify-permissions-test1/amplify-permissions-test1/AWSMobileClient+AWSCognitoUsePoolsAuthProviderAsync.swift
@@ -0,0 +1,26 @@
+//
+//  AWSMobileClient+AWSCognitoUsePoolsAuthProviderAsync.swift
+//  amplify-permissions-test1
+//
+//  Created by Shannon J Hager on 3/14/19.
+//  Copyright © 2019 Skookum, Inc. All rights reserved.
+//
+
+import Foundation
+import AWSAppSync
+import AWSMobileClient
+
+// from https://github.com/awslabs/aws-mobile-appsync-sdk-ios/issues/157#issuecomment-455323169
+// Instead of a separate class, declare a direct conformance
+extension AWSMobileClient: AWSCognitoUserPoolsAuthProviderAsync {
+    public func getLatestAuthToken(_ callback: @escaping (String?, Error?) -> Void) {
+        getTokens { (tokens, error) in
+            if error != nil {
+                callback(nil, error)
+            } else {
+                callback(tokens?.idToken?.tokenString, nil)
+            }
+        }
+    }
+}
+
